3d890e89487d87eb2df0bd78d9b2c2fd241203fe
ShabVl <motohonda250@mail.ru>
2022-11-10 20:09:40 +0300

    Add file

---
+++ test/test_AvarageMark.py
@@ -0,0 +1,53 @@
+from Types import DataType
+from AvarageMark import AvarageMark
+import pytest
+
+RatingsType = dict[str, float]
+
+
+class TestAvarageMark:
+
+    @pytest.fixture()
+    def input_data(self) -> tuple[DataType, RatingsType]:
+        data: DataType = {
+            "Глухов Геннадий Сергеевич":
+                [
+                    ("математика", 95),
+                    ("русский язык", 76),
+                    ("программирование", 90),
+
+                ],
+
+            "Косов Михаил Владимирович":
+                [
+                    ("русский язык", 80),
+                    ("программирование", 78),
+                    ("литература", 97)
+                ],
+
+            "Зотов Степан Олегович":
+                [
+                    ("математика", 76),
+                    ("русский язык", 100),
+                    ("литература", 97)
+                ]
+        }
+
+        rating_scores: RatingsType = {
+            "Глухов Геннадий Сергеевич": 2,
+            "Косов Михаил Владимирович": 1,
+            "Зотов Степан Олегович": 2
+        }
+
+        return data, rating_scores
+
+    def test_init_calc_rating(self, input_data: tuple[DataType,
+                                                      RatingsType]) -> None:
+        calc_rating = AvarageMark(input_data[0])
+        assert input_data[0] == calc_rating.data
+
+    def test_calc(self, input_data: tuple[DataType, RatingsType]) -> None:
+        rating = AvarageMark(input_data[0]).calc()
+        for student in rating:
+            rating_score = rating[student]
+            assert rating_score == input_data[1][student]
---
+++ test/test_JsondataReader.py
@@ -0,0 +1,36 @@
+import pytest
+from typing import Tuple
+from Types import DataType
+from JsondataReader import JsondataReader
+
+
+class TestJsonDataReader:
+
+    @pytest.fixture()
+    def file_and_data_content(self) -> Tuple[str, DataType]:
+        text = '{ "Иванов Константин Дмитриевич": {\n' + \
+            ' "математика": 91, \n' + ' "химия": 100 },\n' + \
+            ' "Петров Петр Семенович": {\n' + \
+            ' "русский язык": 87,\n' + ' "литература": 78 } }'
+
+        data = {
+            "Иванов Константин Дмитриевич": [
+                ("математика", 91), ("химия", 100)
+            ],
+            "Петров Петр Семенович": [
+                ("русский язык", 87), ("литература", 78)
+            ]
+        }
+        return text, data
+
+    @pytest.fixture()
+    def filepath_and_data(self, file_and_data_content:
+                          Tuple[str, DataType],
+                          tmpdir) -> Tuple[str, DataType]:
+        p = tmpdir.mkdir("datadir").join("my_data.json")
+        p.write_text(file_and_data_content[0], encoding='utf-8')
+        return str(p), file_and_data_content[1]
+
+    def test_read(self, filepath_and_data: Tuple[str, DataType]) -> None:
+        file_content = JsondataReader().read(filepath_and_data[0])
+        assert file_content == filepath_and_data[1]
